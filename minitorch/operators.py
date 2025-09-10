e37758a022f8327593c58470f336f41de39da0ed
Gaunov Rustam <gaunr@bk.ru>
2025-09-10 14:48:04 +0300

    first task Done!

--- minitorch/operators.py
+++ minitorch/operators.py
@@ -131,7 +131,7 @@ def inv_back(x: float, d: float) -> float:
 def relu_back(x: float, d: float) -> float:
     r"If $f = relu$ compute $d \times f'(x)$"
     # TODO: Implement for Task 0.1.
-    return x > 0
+    return (x > 0) * 1
     raise NotImplementedError('Need to implement for Task 0.1')
 
 
